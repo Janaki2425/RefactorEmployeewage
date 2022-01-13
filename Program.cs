3857125cbda8358e0041b8c464326c44cd52c0fb
Janu shankar <janakiharishankar24@gmail.com>
2022-01-14 00:14:00 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EmployeewageOOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emppresence employeepresent = new Emppresence();
+            employeepresent.Employee_Check_UC1();
+            Employeewage employee = new Employeewage();
+            employee.Employee_Check_UC2();
+        }
+
+    }
+}
