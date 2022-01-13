9cb49fcf5865db07747765e6c42a240ee85807cb
Janu shankar <janakiharishankar24@gmail.com>
2022-01-14 00:17:33 +0530

    adding file

---
+++ Emppresence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeewageOOPS
+{
+    internal class emppresence
+    {
+        public static void Employee_Check_UC1()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(2);
+            Console.WriteLine(empcheck);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
+
+    
+
